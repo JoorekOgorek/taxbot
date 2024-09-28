127b11f6235b8aaa2bd64f4db39c80d6c9e0cd17
Mike <mike@mbp.local>
2024-09-28 22:33:23 +0200

    xml file

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, request, jsonify, Response
+from flask import Flask, render_template, request, jsonify, Response, send_file
+from io import BytesIO
 
 app = Flask(__name__)
 
@@ -22,8 +23,14 @@ def handle_api():
         # Handle cases where JSON is not provided
         return jsonify({"error": "Invalid JSON data"}), 400
 
+# in the end the model should give the user a link in chat, in a form:
+# <a href="/taxform.xml">Download generated taxform XML</a>
+
 @app.route('/taxform.xml')
 def get_xml():
+    file_data = BytesIO()
+
+
     xmlstring = """<?xml version="1.0" encoding="UTF-8"?>
 <note>
   <to>Tove</to>
@@ -32,7 +39,10 @@ def get_xml():
   <body>Don't forget me this weekend!</body>
 </note>"""
 
-    r = Response(response=xmlstring, status=200, mimetype='text/xml')
-    # r.headers["Content-Type"] = "text/xml; charset=utf-8"
+    file_data.write(xmlstring.encode('utf-8'))
+    file_data.seek(0)
+
+    return send_file(file_data, as_attachment=True, download_name="taxform.xml", mimetype='text/xml')
+
     return r
 
